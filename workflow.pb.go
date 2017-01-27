27e8fa4c96f8063a6609afe4da95acfe31fc3ca3
Akshay <kumarsharmaa69@gmail.com>
2017-01-27 09:59:46 +0530

    Added method to update status to completed and changed request and response name from price prospective to product prospective

--- workflow.pb.go
+++ workflow.pb.go
@@ -10,8 +10,8 @@ It is generated from these files:
 
 It has these top-level messages:
 	Product
-	PriceUpdateRequest
-	PriceUpdateResponse
+	ProductsRequest
+	ProductsResponse
 */
 package workFlowClient
 
@@ -59,32 +59,32 @@ func (m *Product) GetVersion() string {
 	return ""
 }
 
-type PriceUpdateRequest struct {
+type ProductsRequest struct {
 	Products []*Product `protobuf:"bytes,1,rep,name=products" json:"products,omitempty"`
 }
 
-func (m *PriceUpdateRequest) Reset()                    { *m = PriceUpdateRequest{} }
-func (m *PriceUpdateRequest) String() string            { return proto.CompactTextString(m) }
-func (*PriceUpdateRequest) ProtoMessage()               {}
-func (*PriceUpdateRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (m *ProductsRequest) Reset()                    { *m = ProductsRequest{} }
+func (m *ProductsRequest) String() string            { return proto.CompactTextString(m) }
+func (*ProductsRequest) ProtoMessage()               {}
+func (*ProductsRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
-func (m *PriceUpdateRequest) GetProducts() []*Product {
+func (m *ProductsRequest) GetProducts() []*Product {
 	if m != nil {
 		return m.Products
 	}
 	return nil
 }
 
-type PriceUpdateResponse struct {
+type ProductsResponse struct {
 	Message string `protobuf:"bytes,1,opt,name=message" json:"message,omitempty"`
 }
 
-func (m *PriceUpdateResponse) Reset()                    { *m = PriceUpdateResponse{} }
-func (m *PriceUpdateResponse) String() string            { return proto.CompactTextString(m) }
-func (*PriceUpdateResponse) ProtoMessage()               {}
-func (*PriceUpdateResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+func (m *ProductsResponse) Reset()                    { *m = ProductsResponse{} }
+func (m *ProductsResponse) String() string            { return proto.CompactTextString(m) }
+func (*ProductsResponse) ProtoMessage()               {}
+func (*ProductsResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
-func (m *PriceUpdateResponse) GetMessage() string {
+func (m *ProductsResponse) GetMessage() string {
 	if m != nil {
 		return m.Message
 	}
@@ -93,8 +93,8 @@ func (m *PriceUpdateResponse) GetMessage() string {
 
 func init() {
 	proto.RegisterType((*Product)(nil), "workFlowClient.Product")
-	proto.RegisterType((*PriceUpdateRequest)(nil), "workFlowClient.PriceUpdateRequest")
-	proto.RegisterType((*PriceUpdateResponse)(nil), "workFlowClient.PriceUpdateResponse")
+	proto.RegisterType((*ProductsRequest)(nil), "workFlowClient.ProductsRequest")
+	proto.RegisterType((*ProductsResponse)(nil), "workFlowClient.ProductsResponse")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -108,7 +108,8 @@ const _ = grpc.SupportPackageIsVersion4
 // Client API for WorkFlow service
 
 type WorkFlowClient interface {
-	SaveUpdatePriceForApproval(ctx context.Context, in *PriceUpdateRequest, opts ...grpc.CallOption) (*PriceUpdateResponse, error)
+	SaveUpdatePriceForApproval(ctx context.Context, in *ProductsRequest, opts ...grpc.CallOption) (*ProductsResponse, error)
+	UpdateStatusToCompleted(ctx context.Context, in *ProductsRequest, opts ...grpc.CallOption) (*ProductsResponse, error)
 }
 
 type workFlowClient struct {
@@ -119,9 +120,18 @@ func NewWorkFlowClient(cc *grpc.ClientConn) WorkFlowClient {
 	return &workFlowClient{cc}
 }
 
-func (c *workFlowClient) SaveUpdatePriceForApproval(ctx context.Context, in *PriceUpdateRequest, opts ...grpc.CallOption) (*PriceUpdateResponse, error) {
-	out := new(PriceUpdateResponse)
+func (c *workFlowClient) SaveUpdatePriceForApproval(ctx context.Context, in *ProductsRequest, opts ...grpc.CallOption) (*ProductsResponse, error) {
+	out := new(ProductsResponse)
 	err := grpc.Invoke(ctx, "/workFlowClient.WorkFlow/SaveUpdatePriceForApproval", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *workFlowClient) UpdateStatusToCompleted(ctx context.Context, in *ProductsRequest, opts ...grpc.CallOption) (*ProductsResponse, error) {
+	out := new(ProductsResponse)
+	err := grpc.Invoke(ctx, "/workFlowClient.WorkFlow/UpdateStatusToCompleted", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -131,7 +141,8 @@ func (c *workFlowClient) SaveUpdatePriceForApproval(ctx context.Context, in *Pri
 // Server API for WorkFlow service
 
 type WorkFlowServer interface {
-	SaveUpdatePriceForApproval(context.Context, *PriceUpdateRequest) (*PriceUpdateResponse, error)
+	SaveUpdatePriceForApproval(context.Context, *ProductsRequest) (*ProductsResponse, error)
+	UpdateStatusToCompleted(context.Context, *ProductsRequest) (*ProductsResponse, error)
 }
 
 func RegisterWorkFlowServer(s *grpc.Server, srv WorkFlowServer) {
@@ -139,7 +150,7 @@ func RegisterWorkFlowServer(s *grpc.Server, srv WorkFlowServer) {
 }
 
 func _WorkFlow_SaveUpdatePriceForApproval_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(PriceUpdateRequest)
+	in := new(ProductsRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -151,7 +162,25 @@ func _WorkFlow_SaveUpdatePriceForApproval_Handler(srv interface{}, ctx context.C
 		FullMethod: "/workFlowClient.WorkFlow/SaveUpdatePriceForApproval",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WorkFlowServer).SaveUpdatePriceForApproval(ctx, req.(*PriceUpdateRequest))
+		return srv.(WorkFlowServer).SaveUpdatePriceForApproval(ctx, req.(*ProductsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _WorkFlow_UpdateStatusToCompleted_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProductsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(WorkFlowServer).UpdateStatusToCompleted(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/workFlowClient.WorkFlow/UpdateStatusToCompleted",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(WorkFlowServer).UpdateStatusToCompleted(ctx, req.(*ProductsRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -164,6 +193,10 @@ var _WorkFlow_serviceDesc = grpc.ServiceDesc{
 			MethodName: "SaveUpdatePriceForApproval",
 			Handler:    _WorkFlow_SaveUpdatePriceForApproval_Handler,
 		},
+		{
+			MethodName: "UpdateStatusToCompleted",
+			Handler:    _WorkFlow_UpdateStatusToCompleted_Handler,
+		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "workflow.proto",
@@ -172,20 +205,21 @@ var _WorkFlow_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("workflow.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 228 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x7c, 0x90, 0x41, 0x4b, 0x03, 0x31,
-	0x10, 0x85, 0x5d, 0x45, 0xdb, 0x1d, 0xc1, 0x43, 0x3c, 0xb8, 0x14, 0x84, 0x25, 0x5e, 0xf6, 0xb4,
-	0x42, 0xfb, 0x0b, 0x54, 0x28, 0xf4, 0x56, 0x22, 0xe2, 0x51, 0x62, 0x33, 0x96, 0x60, 0xdc, 0x89,
-	0x99, 0x74, 0xf7, 0xef, 0x8b, 0x69, 0x2a, 0xac, 0x82, 0xb7, 0xbc, 0xf0, 0xde, 0xfb, 0x1e, 0x03,
-	0x17, 0x03, 0x85, 0xf7, 0x37, 0x47, 0x43, 0xeb, 0x03, 0x45, 0x12, 0x49, 0x2f, 0x1d, 0x0d, 0x0f,
-	0xce, 0x62, 0x17, 0xe5, 0x3d, 0x4c, 0xd6, 0x81, 0xcc, 0x6e, 0x13, 0xc5, 0x35, 0x80, 0xdf, 0x3f,
-	0x5f, 0xac, 0xa9, 0x8a, 0xba, 0x68, 0x4e, 0x55, 0x99, 0x7f, 0x56, 0x46, 0x54, 0x30, 0xe9, 0x31,
-	0xb0, 0xa5, 0xae, 0x3a, 0xae, 0x8b, 0xa6, 0x54, 0x07, 0x29, 0x57, 0x20, 0xd6, 0xc1, 0x6e, 0xf0,
-	0xc9, 0x1b, 0x1d, 0x51, 0xe1, 0xe7, 0x0e, 0x39, 0x8a, 0x05, 0x4c, 0x73, 0x98, 0xab, 0xa2, 0x3e,
-	0x69, 0xce, 0xe7, 0x57, 0xed, 0x18, 0xde, 0x66, 0xb2, 0xfa, 0x31, 0xca, 0x5b, 0xb8, 0x1c, 0x55,
-	0xb1, 0xa7, 0x8e, 0xf1, 0x9b, 0xfd, 0x81, 0xcc, 0x7a, 0x8b, 0x69, 0x57, 0xa9, 0x0e, 0x72, 0xce,
-	0x30, 0x7d, 0xce, 0xa5, 0x62, 0x0b, 0xb3, 0x47, 0xdd, 0xe7, 0x6c, 0xaa, 0x59, 0x52, 0xb8, 0xf3,
-	0x3e, 0x50, 0xaf, 0x9d, 0x90, 0x7f, 0xe9, 0xbf, 0x37, 0xcf, 0x6e, 0xfe, 0xf5, 0xec, 0xc7, 0xc8,
-	0xa3, 0xd7, 0xb3, 0x74, 0xcb, 0xc5, 0x57, 0x00, 0x00, 0x00, 0xff, 0xff, 0x0e, 0x50, 0xdc, 0x6c,
-	0x5d, 0x01, 0x00, 0x00,
+	// 250 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xac, 0x91, 0x41, 0x4b, 0xc3, 0x40,
+	0x10, 0x85, 0x8d, 0xa2, 0x6d, 0x46, 0x50, 0xd9, 0x4b, 0x43, 0x41, 0x0c, 0x39, 0xe5, 0x20, 0x39,
+	0xb4, 0xbf, 0x40, 0x0b, 0x01, 0x6f, 0x65, 0xab, 0x78, 0x53, 0xd6, 0xee, 0x28, 0xc1, 0x6d, 0x66,
+	0xdd, 0xd9, 0x24, 0xbf, 0xd2, 0xff, 0x24, 0x26, 0x1b, 0xc5, 0x43, 0x6f, 0xde, 0xf6, 0x2d, 0xef,
+	0x7d, 0xef, 0xc1, 0xc0, 0x59, 0x47, 0xee, 0xfd, 0xd5, 0x50, 0x57, 0x58, 0x47, 0x9e, 0x44, 0xaf,
+	0x4b, 0x43, 0xdd, 0xca, 0x54, 0x58, 0xfb, 0xec, 0x16, 0x26, 0x6b, 0x47, 0xba, 0xd9, 0x7a, 0x71,
+	0x09, 0x60, 0x87, 0xe7, 0x73, 0xa5, 0x93, 0x28, 0x8d, 0xf2, 0x63, 0x19, 0x87, 0x9f, 0x3b, 0x2d,
+	0x12, 0x98, 0xb4, 0xe8, 0xb8, 0xa2, 0x3a, 0x39, 0x4c, 0xa3, 0x3c, 0x96, 0xa3, 0xcc, 0x4a, 0x38,
+	0x0f, 0x0c, 0x96, 0xf8, 0xd1, 0x20, 0x7b, 0xb1, 0x84, 0x69, 0x48, 0x72, 0x12, 0xa5, 0x47, 0xf9,
+	0xe9, 0x62, 0x56, 0xfc, 0x6d, 0x2e, 0x42, 0x44, 0xfe, 0x18, 0xb3, 0x6b, 0xb8, 0xf8, 0xe5, 0xb0,
+	0xa5, 0x9a, 0xf1, 0xbb, 0x75, 0x87, 0xcc, 0xea, 0x0d, 0xfb, 0x45, 0xb1, 0x1c, 0xe5, 0xe2, 0x33,
+	0x82, 0xe9, 0x63, 0x40, 0x0a, 0x05, 0xf3, 0x8d, 0x6a, 0xf1, 0xc1, 0x6a, 0xe5, 0x71, 0xed, 0xaa,
+	0x2d, 0x96, 0xe4, 0x6e, 0xac, 0x75, 0xd4, 0x2a, 0x23, 0xae, 0xf6, 0x74, 0x8f, 0x73, 0xe7, 0xe9,
+	0x7e, 0xc3, 0xb0, 0x23, 0x3b, 0x10, 0x4f, 0x30, 0x1b, 0xf0, 0x1b, 0xaf, 0x7c, 0xc3, 0xf7, 0xb4,
+	0xa2, 0x9d, 0x35, 0xe8, 0x51, 0xff, 0x0b, 0xff, 0xe5, 0xa4, 0x3f, 0xd0, 0xf2, 0x2b, 0x00, 0x00,
+	0xff, 0xff, 0x9f, 0x9e, 0x51, 0xbd, 0xb2, 0x01, 0x00, 0x00,
 }
